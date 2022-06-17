e85a053297735a557f357f88df55f20ce54574b6
308 <308@academicos.cesde.edu.co>
2022-06-17 12:07:36 -0500

    cmbios js funciones

--- src/js/controladorIndex.js
+++ src/js/controladorIndex.js
@@ -1,9 +1,34 @@
-//ENTRADAS == VARIABLES
-var nombre="JAMES RODRIGUEZ"
+//cosntruyendo arreglos
+// 1.forma tradicional
+//un arrgleo vacio seria let numeros=[]
+let numeros=[1,2,3,4,5]
+let nombres=['juan','camila','andres','catalina']
+let votos=[true,true, false, false, true]
 
-if(true){
-    var nombre="Lucho diaz"
+// 2.forma poo
+let numerosPOO=Array(1,2,3,4,5)
+let nombresPOO=Array('juan', 'camila','yuliza','mariana')
+let votosPOO=Array(true,true,false,false,true)
+
+//imprimir los elementos de un arreglo
+console.log(numeros)
+console.log(numeros)
+
+//aceder a un solo elemento del arreglo
+console.log(numeros[3])
+console.log(numeros[4])
+
+//objetos en js
+let persona={
+    nombre:'juan',
+    edad:32,
+    hinchaDelNacional:true
+    foto:'url'
 }
-console.log(`hola ${nombre}`)
-console.log("hola")
-console.log("adios")
+let producto={
+    nombre:'xbox',
+    precio:200000,
+    cantidad:null,
+    foto:'',
+    descripcion:'consosla de videojuegos'
+}
